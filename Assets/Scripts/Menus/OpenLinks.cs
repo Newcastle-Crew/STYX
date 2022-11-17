9162b0f4aa5dd8932d28d9d2800885a2184055e0
SquidGeneral10 <SquidGeneral10GameDev@Gmail.com>
2022-11-17 16:29:36 +0000

    Added the basics

---
+++ Assets/Scripts/Menus/OpenLinks.cs
@@ -0,0 +1,24 @@
+#region 'Using' information
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#endregion
+
+public class OpenLinks : MonoBehaviour
+{
+    // credits for the team 
+
+    public void WestyCredit()
+    { Application.OpenURL("https://westydesign.itch.io/"); }
+
+    public void LawtonCredit()
+    { Application.OpenURL("https://www.instagram.com/lami_made_a_thing/"); }
+
+    public void LeonCredit()
+    { Application.OpenURL("https://dxrkest.itch.io"); }
+
+    public void JamesCredit()
+    { Application.OpenURL("https://github.com/JAMZ295"); }
+
+    // credits for stuff we use will go below here
+}
