18f1c4fa19becae10817009402e0088ce40cb24f
SquidGeneral10 <SquidGeneral10GameDev@Gmail.com>
2022-11-22 16:27:01 +0000

    Cannon prototype working
    Press space when standing behind it to rotate.

---
+++ Assets/Scripts/cannonScript.cs
@@ -0,0 +1,74 @@
+﻿#region 'Using' information
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#endregion
+
+public class cannonScript : MonoBehaviour
+{
+
+    /// Made with this tutorial: https://youtu.be/SDl5YTis__k
+    /// rotations are all me, baby
+
+    // TODO: find a way to make it so the player needs to be near the cannon before it shoots
+    // bonus: find a way to show a 'countdown' on screen before the cannon shoots
+    // bonus: find a way to show the 'cooldown' before the cannon is ready to shoot again
+    
+    public Transform firepoint; // The exact area that the cannonball fires from. Middle.
+
+    public Transform cannonRotation; // keeps track of the cannon's 'x' rotation
+
+    public GameObject bullet; // The cannonball that fires.
+    public GameObject cannon; // the cannon itself, that can be rotated.
+
+    float timeBetween; // the time between shots (probably will be removed)
+    public float startTimeBetween; // begins a countdown between shots
+
+    float timeUntilShoot = 5f; // unused; see first TODO
+
+    float cannonZ = 0; // the cannon's rotation
+
+    private bool readyToGo;
+
+    void Start()
+    { timeBetween = startTimeBetween; } // will likely be removed, best for automatic fire
+
+    void Rotate() // rotates the cannon
+    {
+        if(cannonZ + 45 > 45)
+            cannonZ = -45;
+        else
+            cannonZ += 45;
+
+        transform.rotation = Quaternion.Euler(0, 0, cannonZ);
+    }
+    
+    void Update()
+    {
+        if(timeBetween <= 0)
+        {
+            Instantiate(bullet,firepoint.position,firepoint.rotation);
+            timeBetween = startTimeBetween;
+        }
+        else
+        { timeBetween -= Time.deltaTime; }
+
+        if(readyToGo == true && Input.GetKeyDown(KeyCode.Space))
+        { Rotate(); }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Player")
+        {
+            readyToGo = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) 
+    {
+    if(other.tag == "Player")
+    {
+        readyToGo = false;
+    }
+    }
+}
---
+++ Assets/Scripts/cannonballScript.cs
@@ -0,0 +1,38 @@
+﻿#region 'Using' information
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#endregion
+
+public class cannonballScript : MonoBehaviour
+{
+    public float speed; // how fast the cannonball moves
+    public SpriteRenderer ballSprite; // used to make the ball turn invisible after hitting an enemy
+
+    Rigidbody2D rb;
+    int enemyHealth = 2;
+
+    public bool Blasting; // supposed to signal the Sound Checks script to play sound, doesn't work
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * speed;
+        
+        Destroy(gameObject, 2f); // destroys the cannonball 2 seconds after it's been fired
+
+        ballSprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<simpleEnemy>() != null)
+        {   
+            Destroy(collision.gameObject); // destroys the collisions of the enemy
+
+            ballSprite.color = new Color(1, 1, 1, 0); // red, blue, green, transparency (1 = visible, 0 = invisible)
+
+            Destroy(gameObject); // destroys the cannonball
+        }
+    }        
+}
