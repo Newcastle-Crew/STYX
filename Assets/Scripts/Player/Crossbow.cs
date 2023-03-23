0c179ba38da6facf53752c85a4126522f3d03a73
SquidGeneral10 <SquidGeneral10GameDev@Gmail.com>
2023-03-23 23:46:35 +0000

    Crossbow recharge bar

--- Assets/Scripts/Player/Crossbow.cs
+++ Assets/Scripts/Player/Crossbow.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 #endregion
 
 public class Crossbow : MonoBehaviour
@@ -10,21 +11,36 @@ public class Crossbow : MonoBehaviour
     public Transform firepoint; // The exact area that the arrow fires from.
     float timeBetween; // time between crossbow shots
     public float startTimeBetween; // begins a countdown between shots. Change in inspector.
-
     public PlayerWeaponParent wp;
+
+    public Slider Slider;
+    public Vector3 Offset; // controls where the bar appears in-game
+    public Color Low;
+    public Color High;
+
+    public GameObject self;
+
+    public void ProgressBar()
+    {
+        Slider.gameObject.SetActive(timeBetween < startTimeBetween && timeBetween > 0 && self.activeInHierarchy);
+        Slider.value = timeBetween;
+        Slider.maxValue = 10;
+
+        Slider.fillRect.GetComponentInChildren<Image>().color = Color.Lerp(Low, High, Slider.normalizedValue);
+    }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1)) // pressing 1 on keyboard switches weapons
-        {
-            wp.SwitchToMelee();
-        }
-
-        if (Input.GetKeyDown(KeyCode.Mouse0) && timeBetween <= 0) // when left-click is pressed while player is able to fire
-        {
-            ShootArrow();
-        }
+        Slider.transform.position = Camera.main.WorldToScreenPoint(transform.parent.position + Offset); // keep progress bar above player's head
         timeBetween -= Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Alpha1)) // pressing 1 on keyboard switches weapons
+        { wp.SwitchToMelee(); }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0) && timeBetween <= 0) // when left-click is pressed while player is able to fire, shoot arrow
+        { ShootArrow(); }
+
+        ProgressBar();
     }
 
     private void ShootArrow()
